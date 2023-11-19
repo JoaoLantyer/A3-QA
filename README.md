07df1f910f7dcc7842f903b04ad5951f59ad45b9
JoaoLantyer <joao1lantyer@gmail.com>
2023-11-19 20:49:21 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# meuBaba
+Projeto criado para avaliação A3 da UC de Gestão e Qualidade de Software utilizando Cypress para testes automatizados;
+
+## Componentes:
+
+Carolina França Pimentel de Cirqueira - RA: 12719110721;
+
+Cleisson Claudio Santana dos Santos - RA: 1272227351;
+
+Gabriel Antonio Lopes de Castro - RA: 1272023100;
+
+Paulo Henrique Pereira Araujo Piedade - RA: 12722116801;
+
+Luís André Gomes De Oliveira Silva - RA: 12722131007;
+
+João Amaral Lantyer RA: 1271919682;
+
+## Como rodar os testes:
+
+1. Primeiramente será necessário instalar o **Visual Studio Code** e instalar a extensão **Live Server**;
+
+2. Também será necessário instalar o **Cypress**, através do comando ```npm install cypress --save-dev``` no terminal;
+
+3. E utilizar do comando `npm install` no terminal do diretório `/meuBaba`;
+
+3. Feito isso, basta ligar o live server do VSCode;
+
+4. Rodar o comando `npx cypress open` no diretório `/meuBaba`. Isto abrirá uma aba do **Cypress**;
+
+5. Agora, basta clicar em **E2E Testing** e escolher o seu navegador de preferência;
+
+6. Uma aba do navegador abrirá e clicaremos em **gui_Login**;
+
+7. Feito isso os testes rodarão em modo automatizado.
+
+Gravamos um [vídeo](../Video/Testes-Cypress.mp4) para estas etapas finais.
---
+++ meuBaba/cypress/e2e/login/gui_Login.cy.js
@@ -0,0 +1,60 @@
+/// <reference types="Cypress"/>
+
+describe('Teste funcional de login e cadastro', () => {
+
+    // TST001 - Número de caracteres de CPF
+    it('Deve limitar o campo de CPF a 11 caracteres', () => {
+        cy.visit("http://127.0.0.1:5500/tela-inicial/index.html")
+        cy.get('#cpf').type("12345678901234567890")
+        cy.get('#cpf').should('have.value', '12345678901')
+    })
+
+    // TST002 - Tipo de caracteres para o CPF
+    it('Deve aceitar apenas números no campo de CPF', () => {
+        cy.visit("http://127.0.0.1:5500/tela-inicial/index.html")
+        cy.get('#cpf').type("abcdefghij")
+        cy.get('#cpf').should('have.value', '')
+    })
+
+    // TST010 - Validação de Aviso de Login Bem Sucedido ou Não
+    it('Preenchendo formulário e apertando botão deve efetuar login', () => {
+        cy.visit("http://127.0.0.1:5500/tela-inicial/index.html")
+        cy.get('#cpf').type("0123456789")
+        cy.get('#senha').type("padraosenha123")
+        cy.get('#entrar').click()
+        cy.get('#bem-vindo').should('contain', 'Login efetuado com sucesso!')
+    })
+
+    // TST004 - Requisitos de Complexidade de Senha
+    it('Deve verificar se a senha atende aos requisitos de complexidade', () => {
+        cy.visit("http://127.0.0.1:5500/tela-cadastro/index.html")
+
+        // Testa uma senha sem número
+        cy.get('#senha').clear().type('Senha@')
+        cy.get('#senha').invoke('prop', 'validity').should('have.property', 'valid', false)
+
+        // Testa uma senha sem letra maiúscula
+        cy.get('#senha').clear().type('senha@123')
+        cy.get('#senha').invoke('prop', 'validity').should('have.property', 'valid', false)
+
+        // Testa uma senha sem letra minúscula
+        cy.get('#senha').clear().type('SENHA@123')
+        cy.get('#senha').invoke('prop', 'validity').should('have.property', 'valid', false)
+
+        // Testa uma senha sem caractere especial
+        cy.get('#senha').clear().type('Senha123')
+        cy.get('#senha').invoke('prop', 'validity').should('have.property', 'valid', false)
+
+        // Testa uma senha que atende a todos os requisitos de complexidade
+        cy.get('#senha').clear().type('Senha@123')
+        cy.get('#senha').invoke('prop', 'validity').should('have.property', 'valid', true)
+    })
+
+    // TST005 - Validação do E-mail
+    it('Verificar se o email é válido', () => {
+        cy.visit("http://127.0.0.1:5500/tela-cadastro/index.html")
+        cy.get('#email').type("emailfalso@email.com.br")
+        cy.get('#email').should('attr', 'type', 'email')
+    })
+
+})
