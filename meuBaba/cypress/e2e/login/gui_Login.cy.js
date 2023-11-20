61e20e6d99e58657c6497e479e8d1be182f89348
JoaoLantyer <joao1lantyer@gmail.com>
2023-11-19 22:50:17 -0300

    Minor change to Cypress pathing

--- meuBaba/cypress/e2e/login/gui_Login.cy.js
+++ meuBaba/cypress/e2e/login/gui_Login.cy.js
@@ -4,21 +4,21 @@ describe('Teste funcional de login e cadastro', () => {
 
     // TST001 - Número de caracteres de CPF
     it('Deve limitar o campo de CPF a 11 caracteres', () => {
-        cy.visit("http://127.0.0.1:5500/tela-inicial/index.html")
+        cy.visit("http://127.0.0.1:5500/meuBaba/tela-inicial/index.html")
         cy.get('#cpf').type("12345678901234567890")
         cy.get('#cpf').should('have.value', '12345678901')
     })
 
     // TST002 - Tipo de caracteres para o CPF
     it('Deve aceitar apenas números no campo de CPF', () => {
-        cy.visit("http://127.0.0.1:5500/tela-inicial/index.html")
+        cy.visit("http://127.0.0.1:5500/meuBaba/tela-inicial/index.html")
         cy.get('#cpf').type("abcdefghij")
         cy.get('#cpf').should('have.value', '')
     })
 
     // TST010 - Validação de Aviso de Login Bem Sucedido ou Não
     it('Preenchendo formulário e apertando botão deve efetuar login', () => {
-        cy.visit("http://127.0.0.1:5500/tela-inicial/index.html")
+        cy.visit("http://127.0.0.1:5500/meuBaba/tela-inicial/index.html")
         cy.get('#cpf').type("0123456789")
         cy.get('#senha').type("padraosenha123")
         cy.get('#entrar').click()
@@ -27,7 +27,7 @@ describe('Teste funcional de login e cadastro', () => {
 
     // TST004 - Requisitos de Complexidade de Senha
     it('Deve verificar se a senha atende aos requisitos de complexidade', () => {
-        cy.visit("http://127.0.0.1:5500/tela-cadastro/index.html")
+        cy.visit("http://127.0.0.1:5500/meuBaba/tela-cadastro/index.html")
 
         // Testa uma senha sem número
         cy.get('#senha').clear().type('Senha@')
@@ -52,7 +52,7 @@ describe('Teste funcional de login e cadastro', () => {
 
     // TST005 - Validação do E-mail
     it('Verificar se o email é válido', () => {
-        cy.visit("http://127.0.0.1:5500/tela-cadastro/index.html")
+        cy.visit("http://127.0.0.1:5500/meuBaba/tela-cadastro/index.html")
         cy.get('#email').type("emailfalso@email.com.br")
         cy.get('#email').should('attr', 'type', 'email')
     })
